971cf7b91d83791ab8cf1060c075b1ff15626d87
hungerpirat <github@tejaphilipp.de>
2014-04-04 12:35:18 +0200

    added MrBeam Shield to the cpu_map.h

--- cpu_map.h
+++ cpu_map.h
@@ -251,6 +251,93 @@
 
 #endif
 
+
+#ifdef CPU_MAP_ARDUINO_UNO_WITH_MR_BEAM_SHIELD // AVR 328p, for Mr Beam Lasercutter. Supports PWM for laser and tec driver.
+
+  // Serial port pins
+  #define SERIAL_RX USART_RX_vect
+  #define SERIAL_UDRE USART_UDRE_vect
+
+  // Define step pulse output pins. NOTE: All step bit pins must be on the same port.
+  #define STEP_DDR        DDRD
+  #define STEP_PORT       PORTD
+  #define X_STEP_BIT      2  // Uno Digital Pin 2
+  #define Y_STEP_BIT      3  // Uno Digital Pin 3
+  #define Z_STEP_BIT      4  // Uno Digital Pin 4
+  #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
+
+  // Define step direction output pins. NOTE: All direction pins must be on the same port.
+  #define DIRECTION_DDR     DDRD
+  #define DIRECTION_PORT    PORTD
+  #define X_DIRECTION_BIT   5  // Uno Digital Pin 5
+  #define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
+  #define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
+  #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
+
+  #define STEPPERS_DISABLE_DDR    DDRB
+  #define STEPPERS_DISABLE_PORT   PORTB
+  #define STEPPERS_DISABLE_BIT    0  // Uno Digital Pin 8
+  #define STEPPERS_DISABLE_MASK (1<<STEPPERS_DISABLE_BIT)
+
+  // NOTE: All limit bit pins must be on the same port
+  #define LIMIT_DDR       DDRB
+  #define LIMIT_PIN       PINB
+  #define LIMIT_PORT      PORTB
+  #define X_LIMIT_BIT     5  // Uno Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
+  #define Y_LIMIT_BIT     4  // Uno Digital Pin 12
+  #define Z_LIMIT_BIT     3  // Uno Digital Pin 11
+  #define LIMIT_INT       PCIE0  // Pin change interrupt enable pin
+  #define LIMIT_INT_vect  PCINT0_vect 
+  #define LIMIT_PCMSK     PCMSK0 // Pin change interrupt register
+  #define LIMIT_MASK ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
+
+
+  // LASER MODULATION
+  #define SPINDLE_ENABLE_DDR   DDRB
+  #define SPINDLE_ENABLE_PORT  PORTB
+  //#define SPINDLE_ENABLE_BIT   4  // Uno Digital Pin 12
+  #define SPINDLE_ENABLE_BIT   2  // Uno Digital Pin 10
+
+  // TEC MODULATION
+  #define SPINDLE_DIRECTION_DDR   DDRB
+  #define SPINDLE_DIRECTION_PORT  PORTB
+  //#define SPINDLE_DIRECTION_BIT   5  // Uno Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
+  #define SPINDLE_DIRECTION_BIT   1  // Uno Digital Pin 9 
+
+  #define COOLANT_FLOOD_DDR   DDRC
+  #define COOLANT_FLOOD_PORT  PORTC
+  #define COOLANT_FLOOD_BIT   3  // Uno Analog Pin 3
+
+ // Define probe switch input pin.
+  #define PROBE_DDR       DDRC
+  #define PROBE_PIN       PINC
+  #define PROBE_PORT      PORTC
+  #define PROBE_BIT       5  // Uno Analog Pin 5
+  #define PROBE_MASK      (1<<PROBE_BIT)
+
+  // NOTE: Uno analog pins 4 and 5 are reserved for an i2c interface, and may be installed at
+  // a later date if flash and memory space allows.
+  // #define ENABLE_M7  // Mist coolant disabled by default. Uncomment to enable.
+  #ifdef ENABLE_M7
+    #define COOLANT_MIST_DDR   DDRC
+    #define COOLANT_MIST_PORT  PORTC
+    #define COOLANT_MIST_BIT   4 // Uno Analog Pin 4
+  #endif  
+
+  // NOTE: All pinouts pins must be on the same port
+  #define PINOUT_DDR       DDRC
+  #define PINOUT_PIN       PINC
+  #define PINOUT_PORT      PORTC
+  #define PIN_RESET        0  // Uno Analog Pin 0
+  #define PIN_FEED_HOLD    1  // Uno Analog Pin 1
+  #define PIN_CYCLE_START  2  // Uno Analog Pin 2
+  #define PINOUT_INT       PCIE1  // Pin change interrupt enable pin
+  #define PINOUT_INT_vect  PCINT1_vect
+  #define PINOUT_PCMSK     PCMSK1 // Pin change interrupt register
+  #define PINOUT_MASK ((1<<PIN_RESET)|(1<<PIN_FEED_HOLD)|(1<<PIN_CYCLE_START))
+
+#endif
+
 //----------------------------------------------------------------------------------------
 
 /* 
